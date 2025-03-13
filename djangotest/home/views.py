f7969ec69a707db7eb23b2ab1f593bca92785a6d
Pota2s <darrenelisy2@gmail.com>
2025-03-13 12:45:40 +0700

    did things

--- djangotest/home/views.py
+++ djangotest/home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from hashlib import sha256
+from .models import User
 
 def index(request):
     return HttpResponse("Hello, world. You're at the home index.")
 
+def register(request):
+    return HttpResponse("Registration methods here, it should ask for input directly on the page and create a user with the details provided.")
+
+def login(request):
+    return HttpResponse("Login method here, it should ask for input, then validate the user using SHA256 for the password")
 
