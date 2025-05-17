e34c80829f3c5d74378b59ee81db94da8264b53a
Youmizu <145573094+Youmizu@users.noreply.github.com>
2025-05-18 00:35:54 +0700

    Delete Uap-Project-recode directory

--- Uap-Project-recode/djangotest/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import SignUpView
-from .forms import CustomUserCreationForm 
-
-urlpatterns = [
-    path("signup/", SignUpView.as_view(form_class=CustomUserCreationForm), name="signup"),
-]
--- Uap-Project-recode/djangotest/store/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import productView,storeView
-
-urlpatterns = [
-    path("product/<int:product_id>",productView, name="product"),
-    path("<int:store_id>",storeView,name="store")
-    
-]
